9c9f26a9b4063dc06d318717f41da69688d355eb
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-13 00:16:39 +0500

    added register mutation

--- src/module/auth/auth.resolver.ts
+++ src/module/auth/auth.resolver.ts
@@ -1,28 +1,35 @@
-import { Resolver, Mutation, Args } from '@nestjs/graphql';
-import { AuthService } from './auth.service';
-import { CreateUserInput } from './dto/create-user.input';
-import { LoginInput } from './dto/login.input';
-import { Auth } from './entities/auth.entity';
-import { AuthResponse } from './dto/auth-response';
-import { UseGuards } from '@nestjs/common';
-import { AuthGuard } from 'src/common/guards/auth-guard';
-import { RolesGuard } from 'src/common/guards/roles.guard';
-import { Roles } from 'src/common/decorators/roles.decorator';
-import { UserRole } from 'src/common/constants/role';
+import { Resolver, Mutation, Args } from "@nestjs/graphql";
+import { AuthService } from "./auth.service";
+import { CreateUserInput } from "./dto/create-user.input";
+import { LoginInput } from "./dto/login.input";
+import { Auth } from "./entities/auth.entity";
+import { AuthResponse } from "./dto/auth-response";
+import { UseGuards } from "@nestjs/common";
+import { AuthGuard } from "src/common/guards/auth-guard";
+import { RolesGuard } from "src/common/guards/roles.guard";
+import { Roles } from "src/common/decorators/roles.decorator";
+import { UserRole } from "src/common/constants/role";
 
 @Resolver(() => Auth)
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
+  // @Mutation(() => Auth)
+  // registerSuperAdmin(
+  //   @Args("createUserInput") createUserInput: CreateUserInput
+  // ) {
+  //   return this.authService.registerSuperAdmin(createUserInput);
+  // }
+
   @UseGuards(AuthGuard, RolesGuard)
   @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN)
   @Mutation(() => Auth)
-  createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
+  createUser(@Args("createUserInput") createUserInput: CreateUserInput) {
     return this.authService.createUser(createUserInput);
   }
 
   @Mutation(() => AuthResponse)
-  async login(@Args('loginInput') loginInput: LoginInput) {
+  async login(@Args("loginInput") loginInput: LoginInput) {
     const { login, password } = loginInput;
     return this.authService.login(login, password);
   }
