7099cb34b862f7cca871c2813989f265021ff509
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-13 00:17:31 +0500

    import guards

--- src/module/student/student.resolver.ts
+++ src/module/student/student.resolver.ts
@@ -1,15 +1,26 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-import { StudentService } from './student.service';
-import { Student } from './entities/student.entity';
-import { CreateStudentInput } from './dto/create-student.input';
-import { UpdateStudentInput } from './dto/update-student.input';
+import { Resolver, Query, Mutation, Args, Int } from "@nestjs/graphql";
+import { StudentService } from "./student.service";
+import { Student } from "./entities/student.entity";
+import { CreateStudentInput } from "./dto/create-student.input";
+import { UpdateStudentInput } from "./dto/update-student.input";
+import { UseGuards } from "@nestjs/common";
+import { AuthGuard } from "src/common/guards/auth-guard";
+import { RolesGuard } from "src/common/guards/roles.guard";
+import { Roles } from "src/common/decorators/roles.decorator";
+import { UserRole } from "src/common/constants/role";
 
 @Resolver(() => Student)
 export class StudentResolver {
   constructor(private readonly studentService: StudentService) {}
 
+  
+
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN)
   @Mutation(() => Student)
-  createStudent(@Args('createStudentInput') createStudentInput: CreateStudentInput) {
+  createStudent(
+    @Args("createStudentInput") createStudentInput: CreateStudentInput
+  ) {
     return this.studentService.create(createStudentInput);
   }
 
@@ -19,17 +30,26 @@ export class StudentResolver {
   }
 
   @Query(() => Student)
-  findOne(@Args('id', { type: () => Int }) id: number) {
+  findOne(@Args("id", { type: () => Int }) id: number) {
     return this.studentService.findOne(id);
   }
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN)
   @Mutation(() => Student)
-  updateStudent(@Args('updateStudentInput') updateStudentInput: UpdateStudentInput) {
-    return this.studentService.update(updateStudentInput.id, updateStudentInput);
+  updateStudent(
+    @Args("updateStudentInput") updateStudentInput: UpdateStudentInput
+  ) {
+    return this.studentService.update(
+      updateStudentInput.id,
+      updateStudentInput
+    );
   }
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN)
   @Mutation(() => Student)
-  removeStudent(@Args('id', { type: () => Int }) id: number) {
+  removeStudent(@Args("id", { type: () => Int }) id: number) {
     return this.studentService.remove(id);
   }
 }
