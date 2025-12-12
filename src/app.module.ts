dbe44b2a0a2493751853685f67d698840a9f6bc4
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-12 20:51:48 +0500

    import typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from "@nestjs/graphql";
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { join } from "path";
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -13,6 +14,16 @@ import { join } from "path";
       driver: ApolloDriver,
       playground: true,
       autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: String(process.env.DB_PASSWORD as string),
+      database: String(process.env.DB_DATABASE as string),
+      autoLoadEntities: true,
+      synchronize: true,
     }),
   ],
   controllers: [],
