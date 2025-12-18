9a457ada634baaa145108316338d96aa1f160ada
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-18 11:27:58 +0500

    added new mutation

--- src/module/group/group.resolver.ts
+++ src/module/group/group.resolver.ts
@@ -1,35 +1,67 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-import { GroupService } from './group.service';
-import { Group } from './entities/group.entity';
-import { CreateGroupInput } from './dto/create-group.input';
-import { UpdateGroupInput } from './dto/update-group.input';
+import { Resolver, Query, Mutation, Args, Int } from "@nestjs/graphql";
+import { GroupService } from "./group.service";
+import { Group } from "./entities/group.entity";
+import { CreateGroupInput } from "./dto/create-group.input";
+import { UpdateGroupInput } from "./dto/update-group.input";
+import { UseGuards } from "@nestjs/common";
+import { Roles } from "src/common/decorators/roles.decorator";
+import { GqlAuthGuard } from "src/common/guards/auth-guard";
+import { RolesGuard } from "src/common/guards/roles.guard";
+import { UserRole } from "src/common/constants/role";
 
 @Resolver(() => Group)
 export class GroupResolver {
   constructor(private readonly groupService: GroupService) {}
 
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN)
   @Mutation(() => Group)
-  createGroup(@Args('createGroupInput') createGroupInput: CreateGroupInput) {
+  createGroup(@Args("createGroupInput") createGroupInput: CreateGroupInput) {
     return this.groupService.create(createGroupInput);
   }
 
-  @Query(() => [Group], { name: 'group' })
-  findAll() {
-    return this.groupService.findAll();
-  }
+  @Query(() => [Group])
+findAllGroups() {
+  return this.groupService.findAll();
+}
 
-  @Query(() => Group, { name: 'group' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
+
+  @Query(() => Group, { name: "group" })
+  findOne(@Args("id", { type: () => Int }) id: number) {
     return this.groupService.findOne(id);
   }
 
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN)
   @Mutation(() => Group)
-  updateGroup(@Args('updateGroupInput') updateGroupInput: UpdateGroupInput) {
+  updateGroup(@Args("updateGroupInput") updateGroupInput: UpdateGroupInput) {
     return this.groupService.update(updateGroupInput.id, updateGroupInput);
   }
 
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN)
   @Mutation(() => Group)
-  removeGroup(@Args('id', { type: () => Int }) id: number) {
+  removeGroup(@Args("id", { type: () => Int }) id: number) {
     return this.groupService.remove(id);
+  }
+
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN)
+  @Mutation(() => Group)
+  addStudentToGroup(
+    @Args("groupId", { type: () => Int }) groupId: number,
+    @Args("studentId", { type: () => Int }) studentId: number
+  ) {
+    return this.groupService.addStudent(groupId, studentId);
+  }
+
+  @UseGuards(GqlAuthGuard, RolesGuard)
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN)
+  @Mutation(() => Group)
+  removeStudentFromGroup(
+    @Args("groupId", { type: () => Int }) groupId: number,
+    @Args("studentId", { type: () => Int }) studentId: number
+  ) {
+    return this.groupService.removeStudent(groupId, studentId);
   }
 }
